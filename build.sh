445c8c3c04d13b3d839def8049fa4f9e78f8ab2e
GustavoCBRL <gustavo.zaidancabral@gmail.com>
2025-12-18 01:43:18 -0300

    Configure project for Vercel deployment

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Install dependencies
-pip install -r requirements.txt
-
 # Collect static files
-cd cardapioAPIProject
-python manage.py collectstatic --no-input
+python cardapioAPIProject/manage.py collectstatic --no-input
 
 # Make migrations
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
+python cardapioAPIProject/manage.py makemigrations --no-input
+python cardapioAPIProject/manage.py migrate --no-input
