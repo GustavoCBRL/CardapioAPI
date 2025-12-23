cfacb6eb2431686f8db7907d79bf2db3ce8b0342
GustavoCBRL <gustavo.zaidancabral@gmail.com>
2025-12-22 21:26:43 -0300

    Alteração nas categorias dos Itens

--- cardapioAPIProject/cardapio/serializers.py
+++ cardapioAPIProject/cardapio/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
+    categoria_nome = serializers.CharField(source='categoria.nome', read_only=True)
     class Meta:
-        model = Item  
-        fields = ['id', 'nome', 'descricao', 'preco', 'imagem', 'categoria']  # Quais campos expor na API
+        model = Item
+        fields = ['id', 'nome', 'descricao', 'preco', 'imagem', 'categoria_nome']  # Exibe também o nome da categoria
